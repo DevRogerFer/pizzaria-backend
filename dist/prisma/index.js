3b29503464fd7890f7a1a4a72e7dc8e38c32788c
Rogério Fernandes <roferrock@gmail.com>
2025-10-25 23:14:51 -0300

    Updating deploy files

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // importando as tipagens do prisma client
 //import {PrismaClient} from '@prisma/client';
-const client_1 = require("../generated/prisma/client");
+const client_1 = require("@prisma/client");
 // inicializando
 const prismaClient = new client_1.PrismaClient();
 // exportando para ser utilizado em outros arquivos do projeto
--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,6 +1,6 @@
 // importando as tipagens do prisma client
 //import {PrismaClient} from '@prisma/client';
-import {PrismaClient} from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client'
 // inicializando
 const prismaClient = new PrismaClient();
 // exportando para ser utilizado em outros arquivos do projeto
